74d1c3ee797958a74357b6c39024422695dd8b77
Alfaafan Yusufa Achmad <yusufa0509@gmail.com>
2023-03-26 21:22:42 +0700

    [alfaafan] added firebase register logic

---
+++ src/components/form/RegisterForm.jsx
@@ -0,0 +1,57 @@
+import { useRef, useState } from "react";
+import { Form, Button, Container, Alert } from "react-bootstrap";
+import { auth } from "../services/firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+export default function RegisterForm() {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const confirmPasswordRef = useRef();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (passwordRef.current.value !== confirmPasswordRef.current.value) {
+      return setError("Passwords do not match");
+    }
+
+    try {
+      setError("");
+      setLoading(true);
+      await createUserWithEmailAndPassword(auth, emailRef.current.value, passwordRef.current.value);
+    } catch (error) {
+      setError(error.message);
+    }
+
+    setLoading(false);
+  };
+
+  return (
+    <Container className="mt-5">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" ref={emailRef} required />
+        </Form.Group>
+        <Form.Group controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" ref={passwordRef} required />
+        </Form.Group>
+        <Form.Group controlId="confirmPassword">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control type="password" ref={confirmPasswordRef} required />
+        </Form.Group>
+        <Button className="w-100 mt-3" type="submit" variant="warning" disabled={loading}>
+          Register
+        </Button>
+      </Form>
+      {error && (
+        <Alert variant="danger" className="mt-3">
+          {error}
+        </Alert>
+      )}
+    </Container>
+  );
+}
