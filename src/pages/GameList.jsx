8f671c09b0dab9b8723eefa130794eda2dafac5a
Nadhif Fuadi <nadhiffuadi@gmail.com>
2023-03-26 15:43:14 +0700

    Add assets folder

--- src/pages/GameList.jsx
+++ src/pages/GameList.jsx
@@ -1,3 +1,24 @@
+import { FaChevronLeft } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+import "../assets/css/GameList.css";
+
 export default function GameList() {
-  return <></>;
+  return (
+    <div className="wrapper">
+      <div className="container">
+        <div className="header">
+          <Link to="/" className="header-link">
+            <FaChevronLeft /> Back
+          </Link>
+        </div>
+        <div className="content">
+          <div className="page-title">
+            <h1>GAME LIST.</h1>
+          </div>
+          div.
+        </div>
+      </div>
+    </div>
+  );
 }
