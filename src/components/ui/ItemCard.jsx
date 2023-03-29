b0615d7158124e1cc362a82915aff368a4e2b128
Nadhif Fuadi <nadhiffuadi@gmail.com>
2023-03-29 13:04:12 +0700

    Add game list

---
+++ src/components/ui/ItemCard.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+import "../../assets/css/ItemCard.css";
+
+import Image from "../../assets/images/game-list-1.png";
+
+export default function ItemCard({ to, image, title, status }) {
+  return (
+    <div className="item-card-wrapper">
+      <Link to={to} className="card-box-wrapper">
+        <div className="card-box">
+          <img src={image} alt="" />
+        </div>
+      </Link>
+      <Link to="/" className="card-title-wrapper">
+        <p className={`card-title ${status}`}>{title}</p>
+      </Link>
+    </div>
+  );
+}
--- src/pages/GameList.jsx
+++ src/pages/GameList.jsx
@@ -2,22 +2,46 @@ import { FaChevronLeft } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 import "../assets/css/GameList.css";
+import ItemCard from "../components/ui/ItemCard.jsx";
+import Image1 from "../assets/images/game-list-1.png";
+import Image2 from "../assets/images/game-list-2.jpeg";
+import Image3 from "../assets/images/game-list-3.jpg";
 
 export default function GameList() {
   return (
-    <div className="wrapper">
-      <div className="container">
-        <div className="header">
-          <Link to="/" className="header-link">
+    <div className="gl-wrapper">
+      <div className="container gl-container">
+        <header>
+          <Link to="/" className="gl-header">
             <FaChevronLeft /> Back
           </Link>
-        </div>
-        <div className="content">
-          <div className="page-title">
+        </header>
+        <main>
+          <div className="gl-page-title">
             <h1>GAME LIST.</h1>
           </div>
-          div.
-        </div>
+          <div className="gl-content-wrapper">
+            <div className="gl-content-header">
+              <p>Current Trending Games</p>
+              <button>View All</button>
+            </div>
+            <div className="gl-content-list">
+              <ItemCard to="/" image={Image1} title="Rock Paper Scissors" />
+              <ItemCard
+                to="#"
+                image={Image2}
+                title="Coming Soon"
+                status="coming-soon"
+              />
+              <ItemCard
+                to="#"
+                image={Image3}
+                title="Coming Soon"
+                status="coming-soon"
+              />
+            </div>
+          </div>
+        </main>
       </div>
     </div>
   );
